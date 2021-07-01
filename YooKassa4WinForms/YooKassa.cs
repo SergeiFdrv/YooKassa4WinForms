5531ad4f3f2bf7d74664f8394da6b649726e9813
SergeiFdrv <sergfedoro@mail.ru>
2021-07-01 22:35:16 +0300

    Improve YooKassa interaction class. - idempotence keys are now generated randomly - HttpClient is used instead of WebClient - the initial request json has been moved to a file

--- YooKassa4WinForms/YooKassa.cs
+++ YooKassa4WinForms/YooKassa.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,7 @@ namespace YooKassa4WinForms
             Credentials = JsonConverter.DeserializeJson<Dictionary<string, string>>("keys.json");
             Id = Credentials["store_id"];
             Key = Credentials["secret_key"];
+            IdempotenceKey = GetKey(15);
         }
 
         /// <summary>
@@ -34,30 +37,75 @@ namespace YooKassa4WinForms
         private static readonly Dictionary<string, string> Credentials;
         private static readonly string Id;
         private static readonly string Key;
+        private static readonly string IdempotenceKey;
+        internal static readonly char[] chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+        private static string GetKey(int size)
+        {
+            byte[] data = new byte[4 * size];
+            using (var gen = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                gen.GetBytes(data);
+            }
+            StringBuilder key = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                uint rnd = BitConverter.ToUInt32(data, i * 4);
+                long idx = rnd % chars.Length;
+
+                key.Append(chars[idx]);
+            }
+            return key.ToString();
+        }
 
         /// <summary>
-        /// Запрос позволяет передать информацию дял создания объекта платежа
+        /// Запрос позволяет передать информацию для создания объекта платежа
         /// </summary>
         /// <typeparam name="T">В качестве типа получаемого объекта рекомендуется выбрать Payment или Dictionary&lt;string, object&gt;</typeparam>
         /// <returns>Объект платежа в актуальном статусе.</returns>
         public static T CreatePaymentObject<T>() where T : class
         {
             string requestUri = $"https://api.yookassa.ru/v3/payments";
-            HttpWebRequest request = WebRequest.Create(requestUri) as HttpWebRequest;
-            request.Method = "POST";
-            request.Headers.Add(HttpRequestHeader.Authorization,
-                "Basic " + Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes($"{Id}:{Key}")));
-            request.ContentType = "application/json";
-            request.Headers.Add("Idempotence-Key", "1234567890");
-            string content = "{\"amount\": {\"value\": \"100.00\", \"currency\": \"RUB\"}," +
-                "\"capture\": true, " +
-                "\"confirmation\": {\"type\": \"redirect\", '\"return_url\": \"https://www.merchant-website.com/return_url\"}, " +
-                "\"description\": \"Заказ №1\"}";
-            using (Stream stream = request.GetRequestStream())
+            string content = File.ReadAllText("requestPayment.json", Encoding.ASCII);
+            string IdKeyPair = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes($"{Id}:{Key}"));
+
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", IdKeyPair);
+            requestMessage.Headers.Add("Idempotence-Key", IdempotenceKey);
+            requestMessage.Content = new StringContent(content);
+            requestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            #region WebClient
+            // Если бы HttpClient не поддерживался (например, при использовании .NET Framework 4),
+            // вместо него можно было бы использовать WebClient:
+            //HttpWebRequest request = WebRequest.Create(requestUri) as HttpWebRequest;
+            //request.Method = "POST";
+            //request.Headers.Add(HttpRequestHeader.Authorization, "Basic " + IdKeyPair);
+            //request.ContentType = "application/json";
+            //request.Headers.Add("Idempotence-Key", IdempotenceKey);
+            //using (Stream stream = request.GetRequestStream())
+            //{
+            //    stream.SerializeJson(content);
+            //}
+            //try
+            //{
+            //    return request.GetResponse().GetResponseStream().DeserializeJson<T>();
+            //}
+            //catch (WebException e)
+            //{
+            //    throw e;
+            //}
+            #endregion
+
+            var response = httpClient.SendAsync(requestMessage).Result;
+            if (!response.IsSuccessStatusCode)
             {
-                stream.SerializeJson(content);
+                string xDesc = response.Content.ReadAsStreamAsync().Result.DeserializeJson<Dictionary<string, string>>()["description"];
+                throw new HttpRequestException($"{(int)response.StatusCode} {response.StatusCode}: {xDesc}\n{response.Content.ReadAsStringAsync().Result}");
             }
-            return request.GetResponse().GetResponseStream().DeserializeJson<T>();
+            return response.Content.ReadAsStreamAsync().Result.DeserializeJson<T>();
         }
 
         //https://yookassa.ru/developers/api?lang=bash#get_payment
